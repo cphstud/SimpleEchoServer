0ee0d6176b306dc5d0776199a616e269233bcb74
thor <thor@cphbusiness.dk>
2021-03-01 22:31:50 +0100

    save stuff

--- src/main/java/classdemo1/EchoServer.java
+++ src/main/java/classdemo1/EchoServer.java
@@ -3,8 +3,7 @@ package classdemo1;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 
 class MyLoader {
@@ -12,10 +11,11 @@ class MyLoader {
     public MyLoader(List<String> myStuff) {
         this.myStuff = myStuff;
     }
+
     public void myLoading(String uri) throws IOException {
         // load from file-system resource
         String line = "";
-        File f = new File(uri);
+        //File f = new File(uri);
         // load from classloader resource
         ClassLoader classLoader = ClassLoader.getSystemClassLoader();
         InputStream is = classLoader.getResourceAsStream(uri);
@@ -26,15 +26,31 @@ class MyLoader {
         while((line = br.readLine())!=null ) {
             myStuff.add(line);
         }
-
+        br.close();
+        ir.close();
     }
 
+    public void mySaving(String uri, HashMap<Integer, String> input) throws IOException {
+        File f = new File(uri);
+        FileWriter fw = new FileWriter(f);
+        BufferedWriter bw = new BufferedWriter(fw);
+
+        Set<Integer> keys = input.keySet();
+        for (Integer k : keys) {
+            System.out.println("Got " + input.get(k));
+            bw.write(k +" -> " + input.get(k));
+        }
+        bw.close();
+        fw.close();
+    }
 }
 
 public class EchoServer {
     public static final int DEFAULT_PORT = 2345;
 
     public static void main(String[] args) {
+        EchoServer echoServer = new EchoServer();
+        echoServer.runServer();
 
     }
 }
