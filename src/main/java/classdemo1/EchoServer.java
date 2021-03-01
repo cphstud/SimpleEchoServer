78e7d13330c9b810fcf0e0344445bb17468206e2
thor <thor@cphbusiness.dk>
2021-03-01 22:12:48 +0100

    first

--- src/main/java/classdemo1/EchoServer.java
+++ src/main/java/classdemo1/EchoServer.java
@@ -1,10 +1,35 @@
 package classdemo1;
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.Scanner;
+
+
+class MyLoader {
+    List<String> myStuff;
+    public MyLoader(List<String> myStuff) {
+        this.myStuff = myStuff;
+    }
+    public void myLoading(String uri) throws IOException {
+        // load from file-system resource
+        String line = "";
+        File f = new File(uri);
+        // load from classloader resource
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        InputStream is = classLoader.getResourceAsStream(uri);
+        // from stream to reader
+        InputStreamReader ir = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(ir);
+
+        while((line = br.readLine())!=null ) {
+            myStuff.add(line);
+        }
+
+    }
+
+}
 
 public class EchoServer {
     public static final int DEFAULT_PORT = 2345;
---
+++ src/test/java/classdemo1/MyLoaderTest.java
@@ -0,0 +1,23 @@
+package classdemo1;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLoaderTest {
+    MyLoader ml;
+    List<String> input;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        input = new ArrayList<>();
+        ml = new MyLoader(input);
+    }
+
+    @org.junit.jupiter.api.Test
+    void myLoader() throws IOException {
+        ml.myLoading("Test");
+    }
+}
